dfae66f48d9058591e9c3f8b9ef6b08ac907b649
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-22 13:11:27 -0800

    dynamically add a child h4 to each counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component{
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ['tag1','tag2','tag3']
   };
 
   handleIncrement = (e) => {
     console.log(e)
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
   };
-
-  doHandleIncrement = () => {
-    this.handleIncrement({id: 1});
-
-  }
 
 //if you use arrow functions you never need to bind 'this'
 // you can use a callback within an onClick to eleminate the need for creating a 'handler' function
@@ -23,12 +18,12 @@ class Counter extends Component{
 
   render(){
 
+    console.log(this.props);
     return (
     <div>
-       <span className={this.getBadgeClasses()}> {this.formatCount()} </span> <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>;
-      <ul>
-        {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-      </ul>
+      {this.props.children}
+       <span className={this.getBadgeClasses()}> {this.formatCount()} </span> <button onClick={() => this.handleIncrement("puppy")} className="btn btn-secondary btn-sm">Increment</button>
+
     </div>
 
   );
@@ -36,14 +31,14 @@ class Counter extends Component{
 
   getBadgeClasses(){
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.state.value === 0) ? "warning" : "primary";
     return classes;
 
   }
 
   formatCount(){
-    const { count }= this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value }= this.state;
+    return value === 0 ? 'Zero' : value;
 
   }
 }
