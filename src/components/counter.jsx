f7a7ee2d3972f756de99780c6be0bce222266f70
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-22 11:19:54 -0800

    conditional render now functional

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,25 +3,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component{
   state = {
-    count: 1
+    count: 0,
+    tags: ['tag1','tag2','tag3']
   };
+
+  renderTags(){
+    if(this.state.tags.length === 0 )
+    return <p>'There are no tags!' </p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
 
 
   render(){
+
     return (
     <div>
-       <span> {this.formatCount()} </span> <button>Increment</button>;
+      {this.renderTags()}
 
     </div>
 
   );
   }
 
-  formatCount(){
-    const { count }= this.state;
-    return count === 0 ? 'Zero' : count;
-
-  }
 }
 
 export default Counter;
