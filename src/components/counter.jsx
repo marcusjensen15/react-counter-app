df8156a938e4c8f3c2c2cd4489967512d2ad65d1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-22 11:52:32 -0800

    using arrow notation in on click to eliminate handler function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,21 +7,25 @@ class Counter extends Component{
     tags: ['tag1','tag2','tag3']
   };
 
-  handleIncrement = () => {
+  handleIncrement = (e) => {
+    console.log(e)
+    this.setState({count: this.state.count + 1});
+  };
 
-
-    this.setState({count: this.state.count + 1})
+  doHandleIncrement = () => {
+    this.handleIncrement({id: 1});
 
   }
 
 //if you use arrow functions you never need to bind 'this'
+// you can use a callback within an onClick to eleminate the need for creating a 'handler' function
 
 
   render(){
 
     return (
     <div>
-       <span className={this.getBadgeClasses()}> {this.formatCount()} </span> <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>;
+       <span className={this.getBadgeClasses()}> {this.formatCount()} </span> <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>;
       <ul>
         {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
       </ul>
